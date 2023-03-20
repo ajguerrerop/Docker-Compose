e785b807a5d67bb0df62f7aa4723bac4afb59828
ajguerrerop <42006744+ajguerrerop@users.noreply.github.com>
2023-03-20 17:49:11 +0100

    Add files via upload

---
+++ Bye_Server/src/main/java/com/gpalex/byeserver/ByeServerApplication.java
@@ -0,0 +1,34 @@
+package com.gpalex.byeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
+@SpringBootApplication
+@RestController
+public class ByeServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ByeServerApplication.class, args);
+  }
+
+
+  @Operation(summary = "Retrieve user and password")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Found the credentials", content = @Content),
+      @ApiResponse(responseCode = "400", description = "Invalid id supplied", content = @Content),
+      @ApiResponse(responseCode = "404", description = "Book not found", content = @Content) }) // @formatter:on
+
+  @GetMapping(value = "/bye", produces = MediaType.TEXT_PLAIN_VALUE)
+  public String whoami() {
+    return String.format("Bye Bye");
+  }
+
+}
